8e3fddfc3729c6e2cbc59edb741720dad1083c1d
[asdfwefsf] <bethel5308@naver.com>
2024-04-09 19:35:12 +0900

    delete unUse Aalrm 관련 file

--- app/src/main/java/com/company/starttoday/Domain/Alarm/AlarmScheduler.kt
+++
@@ -1,8 +0,0 @@
-package com.company.starttoday.Domain.Alarm
-
-import com.company.starttoday.Domain.Alarm.Model.DomainDTO
-
-interface AlarmScheduler {
-    fun schedule(item: DomainDTO)
-    fun cancel(item: DomainDTO)
-}
