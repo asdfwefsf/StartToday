5c9255bfefeba7303c2087bfd720fe4403c022f8
[asdfwefsf] <bethel5308@naver.com>
2024-04-28 01:56:17 +0900

    2024-04-28 start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.hilt:hilt-common:1.2.0")
-    implementation("androidx.hilt:hilt-work:1.2.0")
 
     testImplementation(Deps.JUNIT)
 
@@ -128,6 +127,9 @@ dependencies {
 
 
     implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.hilt:hilt-work:1.2.0")
+
+
     kapt ("androidx.hilt:hilt-compiler:1.2.0")
 
 
